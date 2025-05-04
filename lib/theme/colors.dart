1ec16ca8cbcbbcd3fa593e99c652d7201729ce5a
JoshiParSa <joshiparsa@gmail.com>
2025-05-04 04:10:05 -0500

    color static class

---
+++ lib/theme/colors.dart
@@ -0,0 +1,46 @@
+import 'dart:ui';
+
+class Colors {
+  static Color primary = Color(0xff5E5EB3);
+  static Color secondary = Color(0xffCEE991);
+  static Color tertiary = Color(0xff9fbaf1);
+
+  static Color success = Color(0xff34b248);
+  static Color warning = Color(0xffe8a530);
+  static Color error = Color(0xffd06c6c);
+
+  static Color primary_100 = Color(0xffeeeef7);
+  static Color primary_200 = Color(0xffcbcbe7);
+  static Color primary_300 = Color(0xffa8a8d6);
+  static Color primary_400 = Color(0xff7474be);
+  static Color primary_500 = Color(0xff5e5eb3);
+  static Color primary_600 = Color(0xff49499c);
+  static Color primary_700 = Color(0xff393979);
+  static Color primary_800 = Color(0xff292957);
+  static Color primary_900 = Color(0xff181834);
+
+  static Color neutral_100 = Color(0xfffbfbfb);
+  static Color neutral_200 = Color(0xffd9dce2);
+  static Color neutral_300 = Color(0xff9aa2b1);
+  static Color neutral_400 = Color(0xff0c0c0c);
+
+  static Color secondary_100 = Color(0xfff6fbea);
+  static Color secondary_200 = Color(0xffe3f2bf);
+  static Color secondary_300 = Color(0xffcee991);
+
+  static Color tertiary_100 = Color(0xffd2dff9);
+  static Color tertiary_200 = Color(0xff9fbaf1);
+  static Color tertiary_300 = Color(0xff799fec);
+
+  static Color success_100 = Color(0xffddf2e0);
+  static Color success_200 = Color(0xff99d8a3);
+  static Color success_300 = Color(0xff34b248);
+
+  static Color warning_100 = Color(0xfffbf0dc);
+  static Color warning_200 = Color(0xfff3d297);
+  static Color warning_300 = Color(0xffe8a530);
+
+  static Color error_100 = Color(0xfff3d8d8);
+  static Color error_200 = Color(0xffe09e9e);
+  static Color error_300 = Color(0xffd06c6c);
+}
